0d2a98e8fcec311ba865910097c6f92b54f5082f
Nicole Listerfelt <nlisterfelt@gmail.com>
2023-07-18 13:24:53 -0500

    setInterval

--- js/challenge.js
+++ js/challenge.js
@@ -1,6 +1,7 @@
 const counterObj = document.getElementById('counter')
 let counter = 0
-let pauseBoolean = false
+let pauseBoolean = true
+let interval
 
 document.addEventListener('DOMContentLoaded',()=>{
     const pause = document.getElementById('pause')
@@ -8,14 +9,9 @@ document.addEventListener('DOMContentLoaded',()=>{
     const minus = document.getElementById('minus')
     const heart = document.getElementById('heart')
     const formInput = document.getElementById('comment-form')
-    
-    /*while(pauseBoolean === true){
-            increment()
-    }*/
-    
-    pause.addEventListener('click', e=> {
-        pauseBoolean = !pauseBoolean
-    })
+    startStopIncrement()
+        
+    pause.addEventListener('click', startStopIncrement)
     plus.addEventListener('click', plusFunc)
     minus.addEventListener('click', minusFunc)
     heart.addEventListener('click', heartFunc)
@@ -25,10 +21,6 @@ document.addEventListener('DOMContentLoaded',()=>{
         commentFunc(comment.value)
     })
 })
-
-function increment(){
-    setTimeout(plusFunc(), 1000)
-}
 
 function plusFunc(){
     counter += 1
@@ -50,4 +42,24 @@ function commentFunc(comment){
     const commentLI = document.createElement('li')
     commentLI.innerText = comment
     commentContainer.appendChild(commentLI)
+}
+
+function startStopIncrement(){
+    pauseBoolean = pauseBoolean === false ? true : false
+    if(pauseBoolean === false){
+        startIncrement()
+    } else {
+        stopIncrement()
+    }
+}
+
+function startIncrement(){
+    if(!interval){
+        interval=setInterval(plusFunc, 1000)
+    }
+}
+
+function stopIncrement(){
+    clearInterval(interval)
+    interval = null
 }
